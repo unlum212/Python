1fa568dbf2a0c30ab1843a7567e08577ebc66187
Muhammet Unlu <101879388+unlum212@users.noreply.github.com>
2022-08-11 14:33:47 +0300

    Add files via upload

---
+++ ArithmeticOperators.py
@@ -0,0 +1,74 @@
+#Arithmetic operators are used to perform mathematical operations like addition, subtraction, multiplication and division.
+#Examples
+a = 12
+b = 5
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print(add)
+print(sub)
+print(mul)
+print(div)
+#Output
+'''
+17
+7
+60
+2.4
+'''
+
+#Modulus Operator : In Python, % is the modulus operator. It is used to find the remainder when first operand is divided by the second.
+#Example
+res = a % b
+print(res)
+#Output
+'''
+2
+'''
+
+#Exponentiation Operator : In Python, ** is the exponentiation operator. It is used to raise the first operand to power of second.
+#Example
+c = a ** b
+print(c)
+#Output
+'''
+248832
+'''
+
+#Floor division : In Python, // is used to conduct the floor division. It is used to find the floorof the quotient when first operand is divided by the second.
+#Example
+x = a // b
+print(x)
+#Output
+'''
+2
+'''
+
+# a > b is True
+print(a > b)
+
+# a < b is False
+print(a < b)
+
+# a == b is False
+print(a == b)
+
+# a != b is True
+print(a != b)
+
+# a >= b is True
+print ( a >= b)
+
+# a <= b is False
+print(a <= b )
+
+#Output
+'''
+True
+False
+False
+True
+True
+False
+'''
